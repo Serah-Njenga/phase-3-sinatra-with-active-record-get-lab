ac3f342d8d4a92d8c432c2c5737ce71f8a6c8a35
Serah-Njenga <serah35.nn@gmail.com>
2023-06-04 17:25:52 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
 
+  belongs_to :bakery
+  has_many :baked_goods
+
   # add routes
 
-end
+    get '/bakeries' do
+      content_type 'application/json'
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+  
+    get '/bakeries/:id' do
+      content_type 'application/json'
+      bakery = Bakery.find(params[:id])
+      bakery.to_json(include: :baked_goods)
+    end
+  end
+  
